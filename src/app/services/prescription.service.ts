c6520dfaf31502f77ba3aac129d858e8b02c0e51
Aymen Abdelmoumen Boukhelkhal <a.boukhalkhel@esi-sba.dz>
2021-02-01 19:02:49 +0100

    orders fix

--- src/app/services/prescription.service.ts
+++ src/app/services/prescription.service.ts
@@ -31,21 +31,5 @@ export class PrescriptionService {
       });
     }
 
-  addComment(comment: Comment){
-    return new Promise((resolve, reject) => {
-      this.httpClient.post<PrescriptionResponse>(this.prescriptionURL + 'prescription/addcomment',comment)
-        .subscribe(response => {
-          console.log(response);
-          if (response.status === 200) {
-            resolve(response.prescription);
-          }
-          else {
-            resolve(false);
-          }
-        }, err => {
-          reject(err);
-        });
-      });
-  }
   
 }
